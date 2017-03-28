00f83ca0e15310e872bd80e85989aef908142e39
takumi kimura <juventus0901@gmail.com>
2017-03-28 22:17:06 +0900

    mail修正

--- send.php
+++ send.php
@@ -9,14 +9,14 @@
   }
 
   // 送信者の情報（メールヘッダー）
-  $add_header = "From:muffin.kimura0901@gmail.com\n";
+  $add_header = "From:sample@gmail.com\n";
 
   // 送信者の情報（メールヘッダー）
-  $add_header .= "Reply-to: muffin.kimura0901@gmail.com\n";
+  $add_header .= "Reply-to: sample@gmail.com\n";
   $add_header .= "X-Mailer: PHP/". phpversion();
 
   // 送信エラーの時にエラーメールを返す先
-  $opt = '-f'.'muffin.kimura0901@gmail.com';
+  $opt = '-f'.'sample@gmail.com';
 
 
   // ヒアドキュメント インデントしたりしているとうまく動かない
@@ -41,7 +41,7 @@ HTML;
   //mb_send_mail(送信先メールアドレス,"メールのタイトル","メール本文","メールのヘッダーFromとかリプライとか","送信エラーになったらどこにメール返すんじゃいっ！");
   //5番目の情報は第5引数と呼ばれるものでして、これがないと迷惑メール扱いになることも。
   mb_send_mail($_SESSION['mail'], "[お問い合わせ]確認メール", $message, $add_header, $opt);
-  mb_send_mail('muffin.kimura0901@gmail.com', "お問い合わせがありました", $message, $add_header, $opt);
+  mb_send_mail('sample@gmail.com', "お問い合わせがありました", $message, $add_header, $opt);
 
   // セッションを破棄
   session_destroy();
